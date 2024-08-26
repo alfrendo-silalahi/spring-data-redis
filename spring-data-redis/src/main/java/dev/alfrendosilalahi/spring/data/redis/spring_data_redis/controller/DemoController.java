212c7a2ceafb0dd93e9af304dfd6a4ec9a6a0d7d
alfrendo-silalahi <alfrendos72@gmail.com>
2024-08-27 00:30:43 +0700

    create new module spring data redis (imperative)

---
+++ spring-data-redis/src/main/java/dev/alfrendosilalahi/spring/data/redis/spring_data_redis/controller/DemoController.java
@@ -0,0 +1,30 @@
+package dev.alfrendosilalahi.spring.data.redis.spring_data_redis.controller;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public DemoController(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @PostMapping
+    public String setDataIntoRedis(@RequestParam String data) {
+        redisTemplate.opsForValue().set("data", data);
+        return String.format("Data '%s' stored on Redis", data);
+    }
+
+    @GetMapping
+    public String getDataFromRedis() {
+        String data = redisTemplate.opsForValue().get("data");
+        return String.format("Data from Redis :: %s", data);
+    }
+
+}
---
+++ spring-data-redis/src/test/java/dev/alfrendosilalahi/spring/data/redis/spring_data_redis/SpringDataRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alfrendosilalahi.spring.data.redis.spring_data_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
